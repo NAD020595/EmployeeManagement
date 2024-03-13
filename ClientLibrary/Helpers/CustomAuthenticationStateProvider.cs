1e3394ce8ea83a0429f7ee02b99af75e8844f093
SD - Nguyen Anh Duy <duy.nguyen1@intranet-trg.com>
2024-03-13 17:03:46 +0700

    Create service to consume the API

---
+++ ClientLibrary/Helpers/CustomAuthenticationStateProvider.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientLibrary.Helpers
+{
+    public class CustomAuthenticationStateProvider(LocalStorageService localStorageService) : AuthenticationStateProvider
+    {
+        private readonly ClaimsPrincipal anonymous = new(new ClaimsPrincipal());
+
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
